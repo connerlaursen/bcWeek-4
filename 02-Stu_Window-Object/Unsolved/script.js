d7b3a01fab754bb507dbf6cf7cac604f8b95716e
Conner Laursen <conner@Conners-MacBook-Air.local>
2022-11-08 18:26:28 -0800

    added day 1 week 4

---
+++ 02-Stu_Window-Object/Unsolved/script.js
@@ -0,0 +1,12 @@
+// TODO: What will the following line of code log? the window object
+console.log(window);
+
+// TODO: What will the following line of code log? the document object, which is the html document in this case
+console.log(window.document);
+
+// TODO: What will the following line of code log? the root element of the document, which is html for html documents
+console.log(document.documentElement);
+
+// TODO: What will the following line of code log? the head section of the html document
+console.log(document.head);
+
---
+++ 08-Stu_Create-Append/Unsolved/script.js
@@ -0,0 +1,51 @@
+var body = document.body;
+var h1El = document.createElement("h1");
+var infoEl = document.createElement("div");
+var imgEl = document.createElement("img");
+var kittenEl = document.createElement("div");
+var nameEl = document.createElement("div");
+var favoriteEl = document.createElement("div");
+// Create ordered list element
+var listEl = document.createElement("ol");
+// Create ordered list items
+var li1 = document.createElement("li");
+var li2 = document.createElement("li");
+var li3 = document.createElement("li");
+var li4 = document.createElement("li");
+
+li1.textContent = "Apples";
+li2.textContent = "Pizza";
+li3.textContent = "Dumplings";
+li4.textContent = "Cupcakes";
+
+// listEl.style.backgroundColor="#333333"; not working??
+
+h1El.textContent = "Welcome to my page";
+kittenEl.textContent = "This is my kitten 🐱.";
+nameEl.textContent = "His name is Jax.";
+favoriteEl.textContent = "My favorite foods are:";
+
+body.appendChild(h1El);
+body.appendChild(infoEl);
+infoEl.appendChild(imgEl);
+infoEl.appendChild(kittenEl);
+infoEl.appendChild(nameEl);
+body.appendChild(favoriteEl);
+favoriteEl.appendChild(listEl);
+// Append ordered list 
+favoriteEl.appendChild(listEl);
+body.appendChild(li1);
+body.appendChild(li2);
+body.appendChild(li3);
+body.appendChild(li4);
+body.appendChild(listEl)
+
+h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+infoEl.setAttribute("style", "margin:auto; width:50%; text-align:center;");
+imgEl.setAttribute("src", "http://placekitten.com/200/300");
+nameEl.setAttribute("style", "font-size:25px; text-align:center;");
+kittenEl.setAttribute("style", "font-size:25px; text-align:center;");
+favoriteEl.setAttribute("style", "font-size:20px;");
+listEl.style.backgroundColor="#333333";
+
+// TODO: Add ordered list items containing four favorite foods
